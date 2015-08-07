fbeff3fc6abe3b33f6fd2456e0622b2dba33a4e1
t207 <269090107@qq.com>
2015-08-07 17:18:51 +0800

    update proposal list style

--- helpers/topic/all.py
+++ helpers/topic/all.py
@@ -64,7 +64,7 @@ class Proposal(topic.Proposal):
         # TODO str to unicode
         result['hCreatedTime'] = str(htime/24/60/60)+'天前' if htime > 24*60*60 else str(htime/60/60)+'小时前' if htime > 60*60 else str(htime/60)+'分钟前' if htime > 60 else '刚刚'
 
-        result['picUrls'] = map(lambda p:'https://dn-geass-images.qbox.me/'+p, record['pickeys'])
+        result['picUrls'] = map(lambda p:{'thumb': 'https://dn-geass-images.qbox.me/'+p+'?imageView2/1/w/200/h/200', 'origin': 'https://dn-geass-images.qbox.me/'+p}, record['pickeys'])
 
         user = Proposal._user.get_one({'_id': Proposal._user.to_objectid(record['auid'])})
         result['author'] = user['nickname']
