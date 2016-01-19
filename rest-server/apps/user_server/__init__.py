81e118d8d2574318b1a50e7fbf03e2748a0bb383
t207 <269090107@qq.com>
2016-01-19 18:43:46 +0800

    add puppet switching

--- rest-server/apps/user_server/__init__.py
+++ rest-server/apps/user_server/__init__.py
@@ -6,6 +6,8 @@ prefix = ''
 
 urls = [
 
+    ('/puppet_list', app.PuppetsHandler),
+
     ('/img/uptoken', app.UploadTokenHandler),
     ('/(|new|topic|proposal|opinion|comment_list|personal|notice_list|following|publish_topics|publish_opinions|help_list|help)', app.PageHandler),
 
